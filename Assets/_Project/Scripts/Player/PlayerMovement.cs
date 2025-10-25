2a5512bd784f53682d79614973954cddc297c045
Crunza17 <crisyolo.cr17@gmail.com>
2025-10-25 14:12:17 +0200

    Feat: Player movement implemented

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();        
+    }
     
+    public void OnMove (InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 newPosition = _rigidbody2D.position + moveInput * (moveSpeed * Time.fixedDeltaTime);
+        _rigidbody2D.MovePosition(newPosition);
+    }
 }
